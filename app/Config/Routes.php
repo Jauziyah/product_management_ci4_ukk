a7adf317d1eed18dc5f7c9a9b54ff0d1c5af8271
Jauziyah <mejaghani@gmail.com>
2025-12-17 10:25:22 +0700

    protecting routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,19 +5,24 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+service('auth')->routes($routes);
+
 $routes->get('/', 'Home::index');
 
 // <-----------------Penjual Page Display------------------>
-$routes->get('/penjual', 'Penjual_display_page::index'); // page dashboard
-$routes->get('/penjual/product', 'Penjual_display_page::product'); // page product
-$routes->get('/penjual/product/edit/(:any)', 'Penjual_display_page::edit_product/$1'); // page edit
-$routes->get('/penjual/product/tambah', 'Penjual_display_page::create_product'); // create product
+$routes->group('penjual', ['filter' => 'session'], function ($routes) {
+    $routes->get('/', 'Penjual_display_page::index');
+    $routes->get('product', 'Penjual_display_page::product');
+    $routes->get('product/edit/(:any)', 'Penjual_display_page::edit_product/$1');
+    $routes->get('product/tambah', 'Penjual_display_page::create_product');
 
-// <-----------------CRUD Method Penjual Produk------------------>
-$routes->get('/penjual/product/(:num)', 'Penjual_product_delete::index/$1'); // delete
-$routes->get('/penjual/product/(:any)', 'Penjual_display_page::detail_produk/$1'); //detail product
-$routes->post('/penjual/product/create', 'Penjual_product_create::index'); // create
-$routes->post('/penjual/product/update/(:num)', 'Penjual_product_update::index/$1'); // edit
+    // <-----------------CRUD Method Penjual Produk------------------>
+    $routes->get('/penjual/product/(:num)', 'Penjual_product_delete::index/$1'); // delete
+    $routes->get('/penjual/product/(:any)', 'Penjual_display_page::detail_produk/$1'); //detail product
+    $routes->post('/penjual/product/create', 'Penjual_product_create::index'); // create
+    $routes->post('/penjual/product/update/(:num)', 'Penjual_product_update::index/$1'); // edit
+});
 
 //  <-----------------Users Page Display------------------>
 $routes->get('/users', 'Penjual_display_page::users');
---
+++ app/Controllers/Auth.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Auth extends BaseController
+{
+    public function index()
+    {
+        return view('auth/login');
+    }
+}
+
+
+?>
